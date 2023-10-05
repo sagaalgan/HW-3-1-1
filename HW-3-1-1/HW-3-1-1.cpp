6382f8466ab3dac3905bbd2454e92f958c428dce
Alexandr <sagaalgan@gmail.com>
2023-10-05 21:47:47 +0300

    first commit123

---
+++ HW-3-1-1/HW-3-1-1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    std::vector<int> numbers = { 4, 7, 9, 14, 12 };
+
+    std::transform(numbers.begin(), numbers.end(), numbers.begin(), [](int num) {
+        return (num % 2 != 0) ? num * 3 : num;
+        });
+
+    std::for_each(numbers.begin(), numbers.end(), [](const int& num) {
+        std::cout << num << " ";
+        });
+    std::cout << std::endl;
+
+    return 0;
+}
